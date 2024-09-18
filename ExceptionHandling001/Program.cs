55d67ba9935b593e1b7e3c0f87facad0489b8b64
rich2023 <110371285+rich2023@users.noreply.github.com>
2024-09-18 17:08:30 +0200

    Add files via upload

---
+++ ExceptionHandling001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = 10;
+                int result = num / 0; // This will throw a DivideByZeroException
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
